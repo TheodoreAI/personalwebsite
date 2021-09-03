e168b3c2e25b07c550ff71541d1755e14f3e1c46
Mateo Estrada <mateoej12@gmail.com>
2021-09-02 18:31:55 -0700

    login, register, logout.

---
+++ passportConfig.js
@@ -0,0 +1,47 @@
+const LocalStrategy = require("passport-local").Strategy;
+const {pool}=require("./dbconfig");
+const bcrypt = require("bcrypt");
+
+function initialize(passport){
+    const authenticateUser = (email, password, done)=>{
+        pool.query('SELECT * FROM admin WHERE email = $1', [email], (err, results)=>{
+            if (err){
+                throw err;
+            }
+            console.log("inside passportCOnfig:", results.rows);
+            if(results.rows.length > 0){
+                const user = results.rows[0];
+                // compare the passwords from database to the one inputted
+                bcrypt.compare(password, user.password, (err, isMatch)=>{
+                    if (err){
+                        throw err;
+                    
+                    }
+                    if(isMatch){
+                        return done(null, user);
+                    }else{
+                        return done(null, false, {message: "Password is not correct"});
+                    }
+                })
+            }else{
+                return done(null, false, {message: "Emai; is not registered!"});
+            }
+
+        })
+    }
+    passport.use(new LocalStrategy({
+        usernameField: "email",
+        passwordField: "password"
+    },
+    authenticateUser));
+    passport.serializeUser((user, done)=>done(null, user.id));
+    passport.deserializeUser((id, done)=>{
+        pool.query(`SELECT * from admin WHERE id = $1`, [id], (err, results)=>{
+            if (err){
+                throw err;
+            }
+            return done(null, results.rows[0]);
+        });
+    });
+}
+module.exports = initialize;
